d30ce065ed23bb6b8f1afc16b16d055f6072b0c2
Shivam14raj <shivamrraj2005@gmail.com>
2026-01-18 11:27:25 +0530

    useReducer

---
+++ React-Hooks/src/App.jsx
@@ -0,0 +1,32 @@
+import { useReducer} from "react";
+import React  from "react"; 
+
+const reducer=(state, action)=>{
+   switch(action.type){
+       case "Increment":
+        return {count: state.count+1, showText: state.showText}
+        case "toggleShowText":
+        return {count: state.count, showText: !state.showText}
+      default: 
+      return state 
+   }
+
+}
+
+ const useReducerHook=  function (){
+  const [state, dispatch]= useReducer(reducer,
+  {count:0, showText: false})
+
+
+   return  <>
+     <div>
+      <h1>{state.count}</h1> 
+   <button onClick={()=>{
+      dispatch({type: "Increment", type:"toggleShowText"})
+   }}>Increment</button> </div>
+    
+    {state.showText && <p>this is text</p>}
+
+   </> 
+}
+ export default useReducerHook; 
